85f91c71e7a063c615f9867ed84cb1a0505504e5
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-08-29 02:10:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(username: params[:username])
-		binding.pry
+		#binding.pry
 		if user
 			redirect "/success"
 		else
